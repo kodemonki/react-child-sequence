008d0bb72e99074d81a3d9d8b58c706e71f62c50
Tom Jane <kodemonki@hotmail.com>
2019-01-22 20:38:27 +0000

    added sequence code

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# react-child-sequence
-React child sequence
+# kodemonki/react-child-sequence
+
+Plays a sequence of child images
+
+## Install
+
+```
+$ npm install @kodemonki/react-child-sequence
+```
+
+## Usage
+
+```js
+const Sequence = require("@kodemonki/react-child-sequence");
+
+<Sequence loop={true} yoyo={true} framerate={60} direcion={"forward"}>
+  <img src={Image1} />
+  <img src={Image2} />
+  <img src={Image3} />
+  <img src={Image4} />
+  <img src={Image5} />
+</Sequence>;
+```
---
+++ index.js
@@ -0,0 +1,132 @@
+import React from "react";
+
+export class Sequence extends React.Component {
+  constructor(props) {
+    super(props);
+    this.intervalId = null;
+    this.fps = this.props.fps != null ? Number(this.props.fps) : 40;
+    this.imagesTotal = -1;
+    this.imagesLoaded = 0;
+    this.direction =
+      this.props.direction != null ? this.props.direction : "forward";
+    this.state = {
+      current: 0
+    };
+    this.updateSequenceBind = this.updateSequence.bind(this);
+    this.updateSequenceYoyoBind = this.updateSequenceYoyo.bind(this);
+  }
+  updateSequence() {
+    if (this.state.current + 1 < this.props.children.length) {
+      this.setState((prevState, props) => ({
+        current: prevState.current + 1
+      }));
+      this.intervalId = setTimeout(this.updateSequenceBind, this.frameRate);
+    } else {
+      this.endSequence();
+    }
+  }
+  updateSequenceYoyo() {
+    if (this.state.current - 1 > 0) {
+      this.setState((prevState, props) => ({
+        current: prevState.current - 1
+      }));
+      this.intervalId = setTimeout(this.updateSequenceYoyoBind, this.frameRate);
+    } else {
+      this.endSequence();
+    }
+  }
+  endSequence() {
+    if (this.props.loop == true) {
+      if (this.props.yoyo == true) {
+        if (this.direction == "forward") {
+          this.direction = "backward";
+          this.setState((prevState, props) => ({
+            current: this.props.children.length - 1
+          }));
+          this.intervalId = setTimeout(
+            this.updateSequenceYoyoBind,
+            this.frameRate
+          );
+        } else if (this.direction == "backward") {
+          this.direction = "forward";
+          this.setState((prevState, props) => ({
+            current: 0
+          }));
+          this.intervalId = setTimeout(this.updateSequenceBind, this.frameRate);
+        }
+      } else {
+        this.setState((prevState, props) => ({
+          current: 0
+        }));
+        this.intervalId = setTimeout(this.updateSequenceBind, this.frameRate);
+      }
+    }
+  }
+  componentWillMount() {
+    if (this.imagesTotal == this.imagesLoaded) {
+      this.startSequence();
+    }
+  }
+  componentWillUnmount() {
+    this.stopSequence();
+  }
+  startSequence() {
+    if (this.intervalId == null) {
+      this.updateSequence();
+    }
+  }
+  stopSequence() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+  getFrame(number) {
+    return this.props.children[number];
+  }
+  handleImageLoaded(e) {
+    this.imagesLoaded++;
+    if (this.imagesLoaded == this.imagesTotal) {
+      this.startSequence();
+    }
+  }
+  renderChildren() {
+    let arr = this.props.children;
+    let newArr = new Array();
+    this.imagesTotal = arr.length;
+    for (let i = 0; i < this.imagesTotal; i++) {
+      if (i == 0) {
+        newArr.push(
+          <img
+            key={i + "first"}
+            className="Sequence__first"
+            style={{ position: "relative", visibility: "hidden" }}
+            src={arr[i].props.src}
+          />
+        );
+      }
+      if (i == this.state.current) {
+        newArr.push(
+          <img
+            key={i}
+            style={{ position: "absolute", visibility: "visible" }}
+            onLoad={this.handleImageLoaded.bind(this)}
+            src={arr[i].props.src}
+          />
+        );
+      } else {
+        newArr.push(
+          <img
+            key={i}
+            style={{ position: "absolute", visibility: "hidden" }}
+            onLoad={this.handleImageLoaded.bind(this)}
+            src={arr[i].props.src}
+          />
+        );
+      }
+    }
+
+    return newArr;
+  }
+  render() {
+    return <div>{this.renderChildren()}</div>;
+  }
+}
